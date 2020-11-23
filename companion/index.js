65614c936a7f329662558dae8b5bed9d8ea1af05
bridgesC648 <70975415+bridgesC648@users.noreply.github.com>
2020-11-22 18:40:23 -0600

    update to companion/index.js

--- companion/index.js
+++ companion/index.js
@@ -1,10 +1,34 @@
 import * as messaging from "messaging";
-import * as fs from "fs";
+import { points } from "../common/waypoints.js";
 import { settingsStorage } from "settings";
 
-//import State from "../app/State";
 
-//let compState = new State();
+messaging.peerSocket.addEventListener("message", (evt) => {
+  let obj = evt.data;
+  settingsStorage.setItem("Waypoint1", obj.Waypoint1);
+  settingsStorage.setItem("Waypoint2", obj.Waypoint2);
+  settingsStorage.setItem("Waypoint3", obj.Waypoint3);
+  settingsStorage.setItem("Waypoint4", obj.Waypoint4);
+  settingsStorage.setItem("Waypoint5", obj.Waypoint5);
+  settingsStorage.setItem("Waypoint6", obj.Waypoint6);
+  settingsStorage.setItem("Waypoint7", obj.Waypoint7);
+  settingsStorage.setItem("Waypoint8", obj.Waypoint8);
+  settingsStorage.setItem("Waypoint9", obj.Waypoint9);
+  settingsStorage.setItem("Waypoint10", obj.Waypoint10);
+  
+  settingsStorage.setItem("newName1", "");
+  settingsStorage.setItem("newName2", "");
+  settingsStorage.setItem("newName3", "");
+  settingsStorage.setItem("newName4", "");
+  settingsStorage.setItem("newName5", "");
+  settingsStorage.setItem("newName6", "");
+  settingsStorage.setItem("newName7", "");
+  settingsStorage.setItem("newName8", "");
+  settingsStorage.setItem("newName9", "");
+  settingsStorage.setItem("newName10", "");
+  
+  console.log(JSON.stringify(evt.data));
+});
 
 // Message socket opens
 messaging.peerSocket.onopen = () => {
