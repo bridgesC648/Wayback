1d5d34a0ce8c2fe91efc4ba6b4e91e3b644bc517
bridgesC648 <70975415+bridgesC648@users.noreply.github.com>
2020-11-29 23:46:44 -0600

    Moved refreshList() to /common/utils.js

--- app/index.js
+++ app/index.js
@@ -28,20 +28,11 @@ import View from "./View";              // Christopher Bridges
 import * as fs from "fs";               // Christopher Bridges
 import { vibration } from "haptics";    // Kevin Le
 
+import {refreshList} from "../common/utils"; // Christopher Bridges
+
 //------------------------------------------------------------------
 // FUNCTIONS
 //------------------------------------------------------------------
-// Christopher Bridges
-/*
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve,ms));
-}*/
-/*
-async function backToNav() {
-  // Method to hackily return to the navigation screen after animation.
-  await sleep(9500);
-  document.history.back().then(view.showNav);
-} */
 
 //sends a vibration and logs the vibration type
 //does not perform if haptics setting is disabled  KL
@@ -87,19 +78,19 @@ view.btnSave.onactivate = function(evt) {
 // Gets called when the Return button gets pressed.
 view.btnReturn.onactivate = function(evt) {
   view.showTiles();
-  refreshList();
+  refreshList(tileList, state, nav);
 }
 
 view.btnConfirmDeletion.onactivate = function(evt) {
   state.delete(deletionIndex);
   view.showTiles();
-  refreshList();
+  refreshList(tileList, state, nav);
   sendMessage();
 }
 
 view.btnCancelDeletion.onactivate = function(evt){
   view.showTiles();
-  refreshList();
+  refreshList(tileList, state, nav);
 }
 
 view.btnConfirmCancelNavigation.onactivate = function(evt) {
@@ -128,39 +119,6 @@ function savePosition(position) {
     console.log("Could not add waypoint: Maximum waypoints reached.");
   }
   sendMessage();
-}
-
-function refreshList(){ // Code to refresh the tile list so it matches the waypoints : Keaton Archibald
-  for (var i = 1; i <= 11; i++){
-    //Repopulates the list's labels and if buttons should be shown.
-    if (state.getAtIndex(i-1) != undefined && tileList[i] != undefined){
-      tileList[i].getElementById("text").text = state.getAtIndex(i-1).getName();
-      tileList[i].getElementById("btnDelete").style.display = "inline";
-    } else if (tileList[i] != undefined){
-      tileList[i].getElementById("text").text = "";
-      tileList[i].getElementById("btnDelete").style.display = "none";
-    } 
-    try {
-      tileList[i].getElementById("btnCancelNavigation").style.display = "none";
-    } catch (err) { }
-  }
-    
-    // Only shows the cancel navigation button if the app is navigating on the waypoint that is being navigated to
-    if (!nav.isNav()) {
-      try {
-        if(state.getCurrent().getName() != ""){
-          tileList[state.getCurrentIndex() + 1].getElementById("btnDelete").style.display = "inline";
-          console.log("no error")
-        }
-      } catch(err) {
-        tileList[state.getCurrentIndex() + 1].getElementById("btnDelete").style.display = "none";
-        console.log(err);
-      }
-      tileList[state.getCurrentIndex() + 1].getElementById("btnCancelNavigation").style.display = "none"; 
-    } else {
-      tileList[state.getCurrentIndex() + 1].getElementById("btnDelete").style.display = "none";
-      tileList[state.getCurrentIndex() + 1].getElementById("btnCancelNavigation").style.display = "inline";
-    }
 }
 
 // Christopher Bridges
@@ -236,7 +194,7 @@ function rename(setKey, txt, evt) {
     sendMessage();
     let stateJSON = fs.readFileSync("state.txt", "cbor");
     state.restoreState(stateJSON);
-    refreshList();
+    refreshList(tileList, state, nav);
   }
 }
 
--- common/utils.js
+++ common/utils.js
@@ -2,3 +2,36 @@ export function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve,ms));
 }
 
+export function refreshList(tileList, state, nav){
+    // Code to refresh the tile list so it matches the waypoints : Keaton Archibald
+    for (var i = 1; i <= 11; i++){
+      //Repopulates the list's labels and if buttons should be shown.
+      if (state.getAtIndex(i-1) != undefined && tileList[i] != undefined){
+        tileList[i].getElementById("text").text = state.getAtIndex(i-1).getName();
+        tileList[i].getElementById("btnDelete").style.display = "inline";
+      } else if (tileList[i] != undefined){
+        tileList[i].getElementById("text").text = "";
+        tileList[i].getElementById("btnDelete").style.display = "none";
+      } 
+      try {
+        tileList[i].getElementById("btnCancelNavigation").style.display = "none";
+      } catch (err) { }
+    }
+      
+      // Only shows the cancel navigation button if the app is navigating on the waypoint that is being navigated to
+      if (!nav.isNav()) {
+        try {
+          if(state.getCurrent().getName() != ""){
+            tileList[state.getCurrentIndex() + 1].getElementById("btnDelete").style.display = "inline";
+            console.log("no error")
+          }
+        } catch(err) {
+          tileList[state.getCurrentIndex() + 1].getElementById("btnDelete").style.display = "none";
+          console.log(err);
+        }
+        tileList[state.getCurrentIndex() + 1].getElementById("btnCancelNavigation").style.display = "none"; 
+      } else {
+        tileList[state.getCurrentIndex() + 1].getElementById("btnDelete").style.display = "none";
+        tileList[state.getCurrentIndex() + 1].getElementById("btnCancelNavigation").style.display = "inline";
+      }
+  }
